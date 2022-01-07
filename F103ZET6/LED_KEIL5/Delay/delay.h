6ae7eaf94cb4d7dfeea6e1a363b88960fee1f4c7
NGUforever <45621458+NGUforever@users.noreply.github.com>
2022-01-07 17:57:13 +0800

    Delete F103ZET6 directory

--- F103ZET6/LED_KEIL5/Delay/delay.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DELAY_H
-#define __DELAY_H
-#include "stm32f10x.h"
-
-void sysclick_init();
-void delay_us(int us);
-void delay_ms(int ms);
-
-#endif
--- F103ZET6/LED_KEIL5/System/Tim/time.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TIME_H
-#define __TIME_H
-
-#include "stm32f10x_tim.h"
-#include "stm32f10x_rcc.h"
-
-void timeInit(int pre,int psc);
-
-#endif
